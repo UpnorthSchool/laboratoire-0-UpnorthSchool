4dc70bde7f8a6f9899b6594ce37897966d820732
UpnorthSchool <kevin.delorme.2@ens.etsmtl.ca>
2025-09-16 15:58:34 -0400

    terminer
    fini reussi

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import logger from 'morgan';
 import flash from 'express-flash-plus';
 
 import { jeuRoutes } from './routes/jeuRouter';
+import { Joueur } from './core/joueur';
 
 // Creates and configures an ExpressJS web server.
 class App {
@@ -61,13 +62,21 @@ class App {
 
     // Route pour classement (stats)
     router.get('/stats', (req, res, next) => {
+
+      const joueurs: Array<Joueur> = JSON.parse(jeuRoutes.controleurJeu.joueurs);
+      //avec aide de copilot et chat gpt pour gerer les NaN 
+      const joueurAvecRatio = joueurs.map(obj => ({ ...obj, ratio: obj.lancers > 0 ? obj.lancersGagnes / obj.lancers : 0 }));
+      //aide pour bien pass/ le map avec le [...joueurAvecRatio]
+      const joueurSorted = [...joueurAvecRatio].sort((n1, n2) => n2.ratio - n1.ratio);
+
+      console.log(joueurSorted);
       res.render('stats',
         // passer objet au gabarit (template) Pug
         {
           title: `${titreBase}`,
           user: user,
           // créer nouveau tableau de joueurs qui est trié par ratio
-          joueurs: JSON.parse(jeuRoutes.controleurJeu.joueurs)
+          joueurs: joueurSorted
         });
     });
 
